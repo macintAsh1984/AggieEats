678b6bef375665366ef288f6d29f057b0accf0cd
macintAsh1984 <ashh.valdez@gmail.com>
2024-11-28 09:43:08 -0800

    Starter files.

---
+++ Aggie Eats/Aggie Eats/Aggie_EatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aggie_EatsApp.swift
+//  Aggie Eats
+//
+//  Created by Ashley Valdez on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Aggie_EatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
