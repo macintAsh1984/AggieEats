9d4993f66b81c33c09501324302392a3134356d5
macintAsh1984 <ashh.valdez@gmail.com>
2024-11-30 19:06:24 -0800

    Created initial skeleton of AggieEats app Home page (including the tab bar).

---
+++ Aggie Eats/Aggie Eats/BottomPageNav.swift
@@ -0,0 +1,18 @@
+//
+//  BottomPageNav.swift
+//  Aggie Eats
+//
+//  Created by Ashley Valdez on 11/30/24.
+//
+
+import SwiftUI
+
+struct BottomPageNav: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomPageNav()
+}
