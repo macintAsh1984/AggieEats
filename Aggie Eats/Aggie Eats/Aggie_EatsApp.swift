6337f0c23497b10502a7b6a38eee8f36e3651f51
macintAsh1984 <ashh.valdez@gmail.com>
2024-12-17 11:43:50 -0800

    Began Stripe integration process (added Stripe package and public key).

--- Aggie Eats/Aggie Eats/Aggie_EatsApp.swift
+++ Aggie Eats/Aggie Eats/Aggie_EatsApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Stripe
 
 @main
 struct Aggie_EatsApp: App {
+    init() {
+        StripeAPI.defaultPublishableKey = "pk_test_51QX5ILHUrE9FgvEhrPlWiY1pkLIKT84xrKxc8XSw5TTo7ifiGGkuUoIz7etHnLh75k08VGyB5IIlyTGMR2YlL4ep00qieijMGC"
+    }
+    
     var body: some Scene {
         WindowGroup {
             BottomPageNav()
